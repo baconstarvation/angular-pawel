602f1e71037ac5d1ddbf101b0864e66ee6c495c3
baconstarvation <ant.ucd@gmail.com>
2014-05-20 08:16:35 -0700

    finish up src login then do login in tests

--- examples/ch7/05_security_module/src/login/toolbar.js
+++ examples/ch7/05_security_module/src/login/toolbar.js
@@ -1,7 +1,7 @@
 angular.module('security.login.toolbar', [])
 
 // The loginToolbar directive is a reusable widget that can show login or logout buttons
-// and information the current authenticated user
+// and information of the current authenticated user
 .directive('loginToolbar', ['security', function(security) {
   var directive = {
     templateUrl: 'security/login/toolbar.tpl.html',
