e5c9bb0ad3997607b369b55d8839cf73aa7ea453
baconstarvation <ant.ucd@gmail.com>
2014-06-13 11:08:32 -0700

    on 06 jquery datepicker directive

---
+++ examples/ch9/05_unique-email_directive/mock-Users.js
@@ -0,0 +1,13 @@
+// We are best to mock up the whole Users object this way because Users
+// relies on so many other services, including the MONGOLAB_CONFIG constant
+angular.module('mock.Users', []).factory('Users', function() {
+	var Users = { };
+	Users.query = function(query, response) {
+		// We capture the repsonse so that the tessts can call it with their own data
+		Users.respondWith = function(emails) {
+			response(emails);
+			Users.respondWith = undefined;
+		};
+	};
+	return Users;
+});
