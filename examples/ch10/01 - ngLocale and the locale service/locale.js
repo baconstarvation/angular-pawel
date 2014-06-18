0b55f862fa415b0dcc9c27d3f46a8251a73e065b
baconstarvation <ant.ucd@gmail.com>
2014-06-17 23:57:21 -0700

    on ch10 01_alert_directive

---
+++ examples/ch10/01 - ngLocale and the locale service/locale.js
@@ -0,0 +1,11 @@
+angular.module('locale', ['ngLocale'])
+	.controller('LocaleCtrl', function ($scope, $locale) {
+
+		$scope.localeid = $locale.id;
+
+		$scope.months = $locale.DATETIME_FORMATS.MONTH;
+	});
+
+
+
+
